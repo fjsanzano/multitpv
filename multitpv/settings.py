46b7438fcf752dbe013b98c7636099bc29f7a20d
Fidel Jimenez Sanzano <fjsanzano85@gmail.com>
2024-12-27 17:17:41 -0500

    [Update] django static

--- multitpv/settings.py
+++ multitpv/settings.py
@@ -117,10 +117,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# incluir en los static files los css y js de base
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static/'),)
+# STATIC_URL = 'static/'
+#
+# # incluir en los static files los css y js de base
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static/'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
