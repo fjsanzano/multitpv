a9c2edcd7f03e5482aa651a9de62c7594f0b3e55
Fidel Jimenez Sanzano <fjsanzano85@gmail.com>
2025-01-03 14:50:52 -0500

    [ADD] Adicionando la creacion y actualizacion de las existencias a traves de la compra y la venta de productos

--- tpv/admin.py
+++ tpv/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import TPV, ProductCategory, ProductProduct, StockQuant, Sale, SaleLine
+from .models import TPV, ProductCategory, ProductProduct, StockQuant, Sale, SaleLine, Purchase, PurchaseLine
 
 
 class ProductAdmin(admin.ModelAdmin):
@@ -10,9 +10,15 @@ class ProductAdmin(admin.ModelAdmin):
 
 
 admin.site.register(TPV)
-admin.site.register(ProductProduct,ProductAdmin)
+admin.site.register(ProductProduct, ProductAdmin)
 admin.site.register(ProductCategory)
-admin.site.register(StockQuant)
+
+
+class StockQuantAdmin(admin.ModelAdmin):
+    list_display = ('tpv_id', 'product_id', 'cant')
+
+
+admin.site.register(StockQuant, StockQuantAdmin)
 
 
 class SaleLineInline(admin.TabularInline):
@@ -27,3 +33,17 @@ class SaleAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Sale, SaleAdmin)
+
+
+class PurchaseLineInline(admin.TabularInline):
+    model = PurchaseLine
+
+
+class Purchasedmin(admin.ModelAdmin):
+    list_display = ('id', 'tpv_id', 'purchase_date')
+    inlines = [
+        PurchaseLineInline,
+    ]
+
+
+admin.site.register(Purchase, Purchasedmin)
--- tpv/models.py
+++ tpv/models.py
@@ -1,9 +1,15 @@
 # Create your models here.
 from django.db import models
 from django.utils.timezone import now
+# from django.utils.translation import ugettext as _
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class TPV(models.Model):
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Punto de venta'
     name = models.CharField(max_length=200)
 
     def __str__(self):
@@ -11,6 +17,10 @@ class TPV(models.Model):
 
 
 class ProductCategory(models.Model):
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Categoría'
+
     name = models.CharField(max_length=200)
 
     def __str__(self):
@@ -18,6 +28,10 @@ class ProductCategory(models.Model):
 
 
 class ProductProduct(models.Model):
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Producto'
+
     name = models.CharField(max_length=200)
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
     image = models.FileField(upload_to='media/uploads/%Y/%m/%d/')
@@ -30,6 +44,9 @@ class ProductProduct(models.Model):
 
 
 class StockQuant(models.Model):
+    class Meta:
+        ordering = ('tpv_id', 'product_id')
+        verbose_name = 'Existencia'
 
     tpv_id = models.ForeignKey(TPV, on_delete=models.CASCADE)
     product_id = models.ForeignKey(ProductProduct, on_delete=models.CASCADE)
@@ -38,8 +55,33 @@ class StockQuant(models.Model):
     def __str__(self):
         return self.tpv_id.name+self.product_id.name
 
+    def update_stock(self, tpv_id, product_id, cant, operation):
+        # buscar si el producto tiene stock en ese punto de venta
+        stock = StockQuant.objects.filter(product_id=product_id, tpv_id=tpv_id)
+        # buscar tpv
+        tpv = TPV.objects.get(id=tpv_id)
+        product = ProductProduct.objects.get(id=product_id)
+        # si hay stock entonces se actualiza
+        if stock:
+            # si la operacion es una entradase suma la cantidad
+            if operation == 'in':
+                new_cant = stock[0].cant+cant
+                # stock[0].objects.update(cant=new_cant)
+                StockQuant.objects.filter(id=stock[0].pk).update(cant=new_cant)
+            else:
+                # si la operacion es una venta se resta la cantidad
+                new_cant = stock[0].cant - cant
+                # stock[0].objects.update(cant=new_cant)
+                StockQuant.objects.filter(id=stock[0].pk).update(cant=new_cant)
+        else:
+            # si no hay stock se crea el stock para ese punto de venta
+            StockQuant.objects.create(tpv_id=tpv, product_id=product, cant=cant)
+
 
 class Sale(models.Model):
+    class Meta:
+        ordering = ('tpv_id', 'sale_date')
+        verbose_name = 'Venta'
     tpv_id = models.ForeignKey(TPV, on_delete=models.CASCADE)
     sale_date = models.DateTimeField("Fecha de venta", default=now)
     productos = models.ManyToManyField(ProductProduct, through='SaleLine')
@@ -53,3 +95,38 @@ class SaleLine(models.Model):
     product_id = models.ForeignKey(ProductProduct, on_delete=models.CASCADE)
     cant = models.IntegerField(default=1)
     precio_venta = models.IntegerField(default=100)
+
+    def save(self, *args, **kwargs):
+        res = super().save(*args, **kwargs)
+
+        StockQuant.update_stock(SaleLine, tpv_id=self.sale_id.tpv_id.pk, product_id=self.product_id.pk,
+                                cant=self.cant, operation='out')
+        return res
+
+
+class Purchase(models.Model):
+    class Meta:
+        ordering = ('tpv_id', 'purchase_date')
+        verbose_name = 'Compra'
+
+    tpv_id = models.ForeignKey(TPV, on_delete=models.CASCADE)
+    purchase_date = models.DateTimeField("Fecha de compra", default=now)
+    productos = models.ManyToManyField(ProductProduct, through='PurchaseLine')
+
+    def __str__(self):
+        return 'Compra ' + str(self.id) + ' ' + self.tpv_id.name + ' del ' + self.purchase_date.strftime(
+            '%Y-%m-%d %H:%M')
+
+
+class PurchaseLine(models.Model):
+    purchase_id = models.ForeignKey(Purchase, on_delete=models.CASCADE)
+    product_id = models.ForeignKey(ProductProduct, on_delete=models.CASCADE)
+    cant = models.IntegerField(default=1)
+    precio_compra = models.IntegerField(default=100)
+
+    def save(self, *args, **kwargs):
+        res = super().save(*args, **kwargs)
+
+        StockQuant.update_stock(PurchaseLine, tpv_id=self.purchase_id.tpv_id.pk, product_id=self.product_id.pk,
+                                cant=self.cant, operation='in')
+        return res
